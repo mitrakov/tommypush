32721dbee7face77cfd56b437a9c2c9d6b8b0095
mitrakov <mitrakov-artem@yandex.ru>
2022-11-13 18:39:28 +0400

    Add simple implementation for USD->RUB rate checker

--- server/src/main/scala/com/mitrakov/self/serverpush/Main.scala
+++ server/src/main/scala/com/mitrakov/self/serverpush/Main.scala
@@ -1,25 +1,80 @@
 package com.mitrakov.self.serverpush
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.messaging._
+import com.google.firebase.messaging.{ApnsConfig, Aps, FirebaseMessaging, Message, Notification}
 import com.google.firebase.{FirebaseApp, FirebaseOptions}
+import com.typesafe.scalalogging.LazyLogging
+import io.burt.jmespath.jackson.JacksonRuntime
+import sttp.client3.{HttpClientSyncBackend, SttpApi}
 import java.io.FileInputStream
+import java.time.{Duration, LocalDateTime}
 
-object Main extends App {
-  val path = "/Users/macbook1/Yandex.Disk.localized/mine/config/firebase/tommypush-firebase-adminsdk.json"
-  val options = FirebaseOptions
-    .builder()
-    .setCredentials(GoogleCredentials.fromStream(new FileInputStream(path)))
-    .setProjectId("tommypush-405b7")
-    .build()
-  val app = FirebaseApp.initializeApp(options)
+object Main extends App with LazyLogging with SttpApi {
+  type MsgId = String
+  if (args.length != 2) {
+    Console.err.println("Usage:   java -jar tommypush.jar <path/to/firebase.json> <usd-rub-rate>\nExample: java -jar tommypush.jar /home/user1/firebase-adminsdk.json 76.2")
+    System.exit(1)
+  }
 
-  val msg = Message
-    .builder()
-    .setNotification(Notification.builder().setTitle("Tommy Push Notification").setBody("Hello, man!").build())
-    .setApnsConfig(ApnsConfig.builder().setAps(Aps.builder().setSound("default").build()).build())
-    .setToken("cVXnVdTcM00lnKcKq4zrBn:APA91bFBMJQ87ryzNipCIVwlXOpwNl-3RWjOTw1Ei3yAFL6q3wr7bkVzRmXMeFYhtFYhEKlfogztMKTTRK4sVBLkLpCGh0NOCicbwqUjF2hJ-shta-lspkZBuTU5MWS6R2-cdBbmYvM1")
-    .build()
-  val msgId = FirebaseMessaging.getInstance(app).send(msg)
-  println(msgId)
+  val firebaseConfPath = args.head
+  val desiredRate = args(1).toDouble
+  var lastSentMsgTime = LocalDateTime.parse("2000-01-01T00:00:00")
+
+  lazy val sttp = HttpClientSyncBackend()
+  lazy val jqRuntime = new JacksonRuntime()
+  lazy val mapper = new ObjectMapper()
+  lazy val firebase = initFirebase(firebaseConfPath)
+
+  while (true) {
+    val minutes = Duration.between(lastSentMsgTime, LocalDateTime.now).toMinutes
+    if (minutes >= 120) {
+      makeRequest() match {
+        case Right(json) =>
+          val realRate = parseDouble(json, jmesPath = "marketdata.data[0][8]")
+          if (realRate >= desiredRate) {
+
+            sendMessage(firebase, "Tommy Push Notification", s"$realRate ≥ $desiredRate!")
+            lastSentMsgTime = LocalDateTime.now()
+          }
+        case Left(error) => logger.error(error)
+      }
+    }
+    Thread.sleep(5000L)
+  }
+
+  def makeRequest(): Either[String, String] = {
+    val response = basicRequest.get(uri"https://iss.moex.com/iss/engines/currency/markets/selt/securities.jsonp?iss.meta=off&iss.only=marketdata&securities=CETS:USD000UTSTOM").send(sttp)
+    val result = response.body
+    logger.debug(result.toString)
+    result
+  }
+
+  def parseDouble(json: String, jmesPath: String): Double = {
+    val jq = jqRuntime.compile(jmesPath)
+    val node = mapper.readTree(json)
+    val result = jq.search(node)
+    logger.info(s"Result is $result")
+    result.asDouble()
+  }
+
+  def initFirebase(secretConfig: String): FirebaseApp = {
+    val options = FirebaseOptions
+      .builder()
+      .setCredentials(GoogleCredentials.fromStream(new FileInputStream(secretConfig)))
+      .setProjectId("tommypush-405b7")
+      .build()
+    FirebaseApp.initializeApp(options)
+  }
+
+  def sendMessage(app: FirebaseApp, title: String, body: String): MsgId = {
+    logger.info("Sending message {}: {}", title, body)
+    val msg = Message
+      .builder()
+      .setNotification(Notification.builder().setTitle(title).setBody(body).build())
+      .setApnsConfig(ApnsConfig.builder().setAps(Aps.builder().setSound("default").build()).build())
+      .setToken("cVXnVdTcM00lnKcKq4zrBn:APA91bFBMJQ87ryzNipCIVwlXOpwNl-3RWjOTw1Ei3yAFL6q3wr7bkVzRmXMeFYhtFYhEKlfogztMKTTRK4sVBLkLpCGh0NOCicbwqUjF2hJ-shta-lspkZBuTU5MWS6R2-cdBbmYvM1")
+      .build()
+    FirebaseMessaging.getInstance(app).send(msg)
+  }
 }
