ec29fa78dd5fe629b732c9940f905b15f7519057
mitrakov <mitrakov-artem@yandex.ru>
2022-11-14 01:23:55 +0400

    Minor: Add some formatting, and extracted some vals

--- server/src/main/scala/com/mitrakov/self/serverpush/Main.scala
+++ server/src/main/scala/com/mitrakov/self/serverpush/Main.scala
@@ -19,6 +19,8 @@ object Main extends App with LazyLogging with SttpApi {
 
   val firebaseConfPath = args.head
   val desiredRate = args(1).toDouble
+  val projectId = "tommypush-405b7"
+  val fcmToken = "cVXnVdTcM00lnKcKq4zrBn:APA91bFBMJQ87ryzNipCIVwlXOpwNl-3RWjOTw1Ei3yAFL6q3wr7bkVzRmXMeFYhtFYhEKlfogztMKTTRK4sVBLkLpCGh0NOCicbwqUjF2hJ-shta-lspkZBuTU5MWS6R2-cdBbmYvM1"
   var lastSentMsgTime = LocalDateTime.parse("2000-01-01T00:00:00")
 
   lazy val sttp = HttpClientSyncBackend()
@@ -59,22 +61,18 @@ object Main extends App with LazyLogging with SttpApi {
   }
 
   def initFirebase(secretConfig: String): FirebaseApp = {
-    val options = FirebaseOptions
-      .builder()
-      .setCredentials(GoogleCredentials.fromStream(new FileInputStream(secretConfig)))
-      .setProjectId("tommypush-405b7")
-      .build()
+    val credentials = GoogleCredentials.fromStream(new FileInputStream(secretConfig))
+    val options = FirebaseOptions.builder().setCredentials(credentials).setProjectId(projectId).build()
+
     FirebaseApp.initializeApp(options)
   }
 
   def sendMessage(app: FirebaseApp, title: String, body: String): MsgId = {
     logger.info("Sending message {}: {}", title, body)
-    val msg = Message
-      .builder()
-      .setNotification(Notification.builder().setTitle(title).setBody(body).build())
-      .setApnsConfig(ApnsConfig.builder().setAps(Aps.builder().setSound("default").build()).build())
-      .setToken("cVXnVdTcM00lnKcKq4zrBn:APA91bFBMJQ87ryzNipCIVwlXOpwNl-3RWjOTw1Ei3yAFL6q3wr7bkVzRmXMeFYhtFYhEKlfogztMKTTRK4sVBLkLpCGh0NOCicbwqUjF2hJ-shta-lspkZBuTU5MWS6R2-cdBbmYvM1")
-      .build()
+    val notification = Notification.builder().setTitle(title).setBody(body).build()
+    val apnsConfig = ApnsConfig.builder().setAps(Aps.builder().setSound("default").build()).build()
+    val msg = Message.builder().setNotification(notification).setApnsConfig(apnsConfig).setToken(fcmToken).build()
+
     FirebaseMessaging.getInstance(app).send(msg)
   }
 }
