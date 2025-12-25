51e07aef614d321921d1cf20bfcd4c50f85f079a
Tommy-Air <tommy@air.ru>
2025-12-25 15:36:03 +0300

    Client: fixed error with Firebase package name (mitrakoff -> mitrakov)

--- server/src/main/java/com/mitrakoff/self/tommypush/Main.java
+++ server/src/main/java/com/mitrakoff/self/tommypush/Main.java
@@ -24,14 +24,15 @@ public class Main {
 }
 
 /* bash example:
-#!/bin/bash
+#!/usr/bin/env bash
 
-fcm1="fHWRTKGHjUnvqSlrBdeqQO:APA91bG0tH-fcy44_OBirgoIt04s4s58k-9gEJPFg9vglCoux9nRdgH7SYMmxUD6HWrTgakeFZK3J6OeZIf2Qs8aaxDUpGRlh9zwBVwWU9Lkg9U5c3MzCYzkrAKf2vRSzr3ClqGPWLbU"
+fcm1="fHWRTKGHjUnvqSlrBdeqQO:APA91bG0tH-fcy44_OBirgoIt04s4s58k-9gEJPFg9vglCoux9nRdgH7SYMmxUD6HWrTgakeFZK3J6OeZIf2Qs8aaxDUpGRlh9zwBVwW"
+fcm2="e_aeRQRJ10W6q2zZzzn6he:APA91bFXh7rQmOBwyDcM8Eiw8Q3qUkEOqNU9chNrAn0YW536XnUk-GTn84vnoc_lROsvTRIxA3Vn0F96t7zuMKqAdAR8VN4-cCdomikH"
 
 JAR=/Users/director/software/tommypush.jar
 CONFIG=/Users/director/Yandex.Disk.localized/all/configs/firebase/tommypush-firebase.json
 TITLE="Tommypush"
 TEXT="Buenas días, Tommy"
 
-java -jar $JAR $CONFIG "$TITLE" "$TEXT" "$fcm1"
+java -jar $JAR $CONFIG "$TITLE" "$TEXT" "$fcm1" "$fcm2"
 */
